b3afaa90e74b23306a8d12016f1f30593102e800
Greg Atemi <magrega7180@gmail.com>
2024-02-27 22:30:54 +0300

    Added CICD config file

--- app/view/demo/CustomWindow.js
+++ app/view/demo/CustomWindow.js
@@ -3,7 +3,7 @@ Ext.define("MsTraining.view.demo.CustomWindow",{
     xtype: 'customwindow',
     title: "My Custom Title",
     height: 200,
-    width: 520,
+    width: 600,
     //autoShow: true,
     closable: true, // enables you to have x on the window 
     maximizable:true,  // []
@@ -11,8 +11,8 @@ Ext.define("MsTraining.view.demo.CustomWindow",{
     draggable:false,
     modal:true
 },function(){
-    // Ext.create("MsTraining.view.demo.CustomWindow");
-   var wd = Ext.create({
+    Ext.create("MsTraining.view.demo.CustomWindow");
+    var wd = Ext.create({
       xtype: "customwindow",
     });
     wd.show();
