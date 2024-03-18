ab1e5e9ffdd2a01558b5ceff8441ecfb57b1eb34
Greg Atemi <magrega7180@gmail.com>
2024-03-18 22:27:30 +0300

    Added auth functionality - Added logged in check for authentication - Formatted the code

--- app/Application.js
+++ app/Application.js
@@ -3,33 +3,36 @@
  * calls Ext.application(). This is the ideal place to handle application launch and
  * initialization details.
  */
-Ext.define(
-    "MsTraining.Application",
-    {
-      extend: "Ext.app.Application",
-  
-      name: "MsTraining",
-  
-      quickTips: false,
-      platformConfig: {
-        desktop: {
-          quickTips: true,
-        },
-      },
-  
-      onAppUpdate: function () {
-        Ext.Msg.confirm(
-          "Application Update",
-          "This application has an update, reload?",
-          function (choice) {
-            if (choice === "yes") {
-              window.location.reload();
-            }
-          }
-        );
-      },
+Ext.define( "MsTraining.Application",
+{
+  extend: "Ext.app.Application",
+
+  name: "MsTraining",
+
+  quickTips: false,
+  platformConfig: {
+    desktop: {
+      quickTips: true,
     },
-    function () {
-      // console.log("On th callback");
-    }
-  );
+  },
+
+  onAppUpdate: function () {
+    Ext.Msg.confirm(
+      "Application Update",
+      "This application has an update, reload?",
+      function (choice) {
+        if (choice === "yes") {
+          window.location.reload();
+        }
+      }
+    );
+  },
+  launch:function(profile){
+    var loggedIn;
+    loggedIn = localStorage.getItem("MsAppLoggedIn");
+    // Ext.widget(loggedIn ? 'app-main' : 'login')
+    Ext.create({
+      xtype: loggedIn ? 'app-main' : 'login'
+    });
+  }
+});
